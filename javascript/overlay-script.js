d047352cc0c05591737ad12495156764fbfe5fa7
Louis Nel <louisnel.me@gmail.com>
2019-12-02 17:08:35 +1300

    update site

--- javascript/overlay-script.js
+++ javascript/overlay-script.js
@@ -3,7 +3,7 @@ var colorArray = [
 	"#BDDFBC",
 	"#FAB89A",
 	"#FCC96C",
-	"#FF934F",
+	"#FFAD7A",
 	"#CEE5F2",
 	"#F1E4E8"
 ];
@@ -34,8 +34,8 @@ $(document).ready(function(){
 		//fade out after 1.5 seconds
 		$(".navbar li").click(function(){
 			$(".page_body").css("background-color", colorArray[Math.floor(Math.random()*colorArray.length)]);
-        	$(".page_body").fadeIn(500);
-        	window.setTimeout(function() {$(".page_body").fadeOut("slow");}, 1500);
+        	setTimeout(function() {$(".page_body").fadeIn(500);}, 200);
+        	setTimeout(function() {$(".page_body").fadeOut("slow");}, 1500);
 		});
 });
 
@@ -44,14 +44,14 @@ $( document ).ready(function() {
 		//info page link function
 		//display info div on click
 		$(".menu_info").click(function() {
-			$(".content_page:not(.info_page)").css("display", "none");
+			$(".content_page").fadeOut(200);
 			setTimeout(function() {$(".info_page").fadeIn(500)}, 2000);
 		});
 
 		//home page link function
 		//displays home on click
 		$(".menu_1883").click(function() {
-			$(".content_page:not(.home_page)").css("display", "none");			
+			$(".content_page").fadeOut(200);			
 			setTimeout(function() {$(".home_page").fadeIn(500)}, 2000);
 			
 		});
@@ -59,7 +59,7 @@ $( document ).ready(function() {
 		//folio page link fuction
 		//display folio page on click
 		$(".menu_folio").click(function() {
-			$(".content_page:not(.menu_folio)").css("display", "none");
+			$(".content_page").fadeOut(200);
 			setTimeout(function() {$(".folio_page").fadeIn(500)}, 2000);
 		});
 });
