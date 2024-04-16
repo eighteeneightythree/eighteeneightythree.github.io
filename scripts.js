d2d00c4fbaee67dbf3a87606c261c694dbac4f4a
eighteeneightythree <48271257+eighteeneightythree@users.noreply.github.com>
2024-04-16 19:42:56 +1200

    update archive source

--- scripts.js
+++ scripts.js
@@ -2,22 +2,22 @@
 
 /* If browser back button was used, flush cache */
 (function () {
-	window.onpageshow = function(event) {
-		if (event.persisted) {
-			//window.location.reload();
+    window.onpageshow = function (event) {
+        if (event.persisted) {
+            //window.location.reload();
             $("#container").fadeIn(500);
-		}
-	};
+        }
+    };
 })();
 
 function camelCaseToSentenceCase(str) {
-	// Split the string by capital letters
-	const words = str.replace(/([a-z])([A-Z])/g, '$1 $2').split(/(?=[A-Z])/);
+    // Split the string by capital letters
+    const words = str.replace(/([a-z])([A-Z])/g, '$1 $2').split(/(?=[A-Z])/);
 
-	// Capitalize the first letter of each word and join them with spaces
-	const sentenceCase = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    // Capitalize the first letter of each word and join them with spaces
+    const sentenceCase = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 
-	return sentenceCase;
+    return sentenceCase;
 }
 
 
@@ -25,7 +25,7 @@ function camelCaseToSentenceCase(str) {
 
 function navigate(url) {
     //fade out page, then redirect
-    $('#container').fadeOut(300, function() {
+    $('#container').fadeOut(300, function () {
         window.location.href = url;
     });
 }
@@ -36,7 +36,7 @@ function generateArchiveTable(data) {
     //loop through 'data' objects (JSON)
     for (let i = 0; i < data.length; i++) {
         //(repair on deployment)
-        let table= document.getElementById("archiveTable");
+        let table = document.getElementById("archiveTable");
         //generate table row from each object in 'data'
         let row = `
         <tr class="archiveTableBody">
@@ -52,19 +52,19 @@ function generateArchiveTable(data) {
         //add genrated 'row' to 'table'
         //(repair on deployment)
         table.innerHTML += row;
-        
+
         //test code (delete on deployment) 
         //table += row;
         //console.log(table);
         //
     }
-	
+
 }
 
 async function displayarchive() {
     //fetch archive.json
     //test fetch url (update on desployment)
-    let archiveFetch = await fetch("https://eighteeneightythree.github.io/dev-repo/archive.json");
+    let archiveFetch = await fetch("https://louisnel.co.nz/archive.json");
     //on promise resolution: JSON -> archiveRecords
     let archiveRecords = await archiveFetch.json();
     //console.log(archiveRecords)
@@ -77,34 +77,34 @@ async function displayarchive() {
 
 function generateArchiveRecord(data) {
     // Iterate over each key-value pair in the JSON object
-	for (const key in data) {
+    for (const key in data) {
         //(repair on deployment)
-		let tableHTML = document.getElementById("itemTable");
-		if (data.hasOwnProperty(key)) {
-			// Create a table row for each key-value pair
+        let tableHTML = document.getElementById("itemTable");
+        if (data.hasOwnProperty(key)) {
+            // Create a table row for each key-value pair
             //(repair on deployment)
-			tableHTML.innerHTML += `<tr><th>${camelCaseToSentenceCase(key)}</th><td>${data[key]}</td></tr>`;
+            tableHTML.innerHTML += `<tr><th>${camelCaseToSentenceCase(key)}</th><td>${data[key]}</td></tr>`;
 
             //test code (delete on deployment)
             //tableHTML +=`<tr><th>${camelCaseToSentenceCase(key)}</th><td>${data[key]}</td></tr>`;
-		}
-	}
+        }
+    }
 }
 
 async function displayArchiveRecord() {
     //(repair on deployment)
-	const searchParams = new URLSearchParams(window.location.search);
-	let archiveFetch = await fetch("https://eighteeneightythree.github.io/dev-repo/archive.json");
-	let archiveRecords = await archiveFetch.json();
-	console.log(archiveRecords);
+    const searchParams = new URLSearchParams(window.location.search);
+    let archiveFetch = await fetch("https://louisnel.co.nz/archive.json");
+    let archiveRecords = await archiveFetch.json();
+    console.log(archiveRecords);
     //console.log(searchParams)
 
-	let recordItem = archiveRecords.find(item => item.accessionNumber === searchParams.get('item'));
-	console.log(recordItem);
+    let recordItem = archiveRecords.find(item => item.accessionNumber === searchParams.get('item'));
+    console.log(recordItem);
 
 
 
-	//generate complete archive record
+    //generate complete archive record
     generateArchiveRecord(recordItem);
 
 }
@@ -117,12 +117,12 @@ displayarchive();
 
 displayArchiveRecord();
 
-$(document).ready(function() {
-	$("#header").fadeIn(500);
-	$("#page_body").fadeIn(1000);
-	$("#footer").fadeIn(1000);
+$(document).ready(function () {
+    $("#header").fadeIn(500);
+    $("#page_body").fadeIn(1000);
+    $("#footer").fadeIn(1000);
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#container").fadeIn(500);
 });
